2c30af0b404141585d196cd5323e79f3d3e9b378
andrewcargill <andycargill01@aol.com>
2022-12-09 10:57:26 +0000

    produce error linked to allow-origin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from 'react-bootstrap/Container';
 import {Route,Switch} from 'react-router-dom';
 import './api/axiosDefaults';
-import SignUpForm from './pages/auth/SignUpFrom';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
