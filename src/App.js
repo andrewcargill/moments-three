2528325ebd964c3776620648d0894c5461e3da59
andrewcargill <andycargill01@aol.com>
2022-12-09 07:56:17 +0000

    Display basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <NavBar />
     </div>
   );
 }
